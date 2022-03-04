1cbf7a0e6d6fcfef6b8aa08371340524fdf24b5f
abc <abc@MacBook-Pro.local>
2022-03-04 12:24:04 -0600

    Updates

---
+++ precache-manifest.82a87ac107c1ce28faaf5515ca1ebe73.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4adbbcc04b8dd87c10659cd73b88fba",
+    "url": "/project-green/index.html"
+  },
+  {
+    "revision": "b46c7f1842b06fa78aa8",
+    "url": "/project-green/static/css/main.54049eb0.chunk.css"
+  },
+  {
+    "revision": "57bd4880fbf825f7b341",
+    "url": "/project-green/static/js/2.b1efc40d.chunk.js"
+  },
+  {
+    "revision": "7aeb96c4878d224a55adce546624b684",
+    "url": "/project-green/static/js/2.b1efc40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46c7f1842b06fa78aa8",
+    "url": "/project-green/static/js/main.1e0d8e01.chunk.js"
+  },
+  {
+    "revision": "afe06b879a66938f7808",
+    "url": "/project-green/static/js/runtime-main.4986cdce.js"
+  },
+  {
+    "revision": "8cccc71bedb39a2e967fdb1e76532b39",
+    "url": "/project-green/static/media/1.8cccc71b.jpg"
+  },
+  {
+    "revision": "103c0884ee13d5bcdeff3dd0d9398f36",
+    "url": "/project-green/static/media/3.103c0884.jpg"
+  },
+  {
+    "revision": "0bbfc705e37a927ce2ae72b749b3154d",
+    "url": "/project-green/static/media/Simple-Line-Icons.0bbfc705.woff"
+  },
+  {
+    "revision": "6ac7320f709ffd2784b4a861e5b60395",
+    "url": "/project-green/static/media/Simple-Line-Icons.6ac7320f.woff2"
+  },
+  {
+    "revision": "b2892aa62b0fb2c21c8d7700e2ef6e56",
+    "url": "/project-green/static/media/Simple-Line-Icons.b2892aa6.ttf"
+  },
+  {
+    "revision": "dacd16c9a3d2cb61b472a35979015bf3",
+    "url": "/project-green/static/media/Simple-Line-Icons.dacd16c9.eot"
+  },
+  {
+    "revision": "de8ad55bfeaebbe67c5b9a362749de1a",
+    "url": "/project-green/static/media/Simple-Line-Icons.de8ad55b.svg"
+  },
+  {
+    "revision": "037ebf694738a833c1c069958582c2a0",
+    "url": "/project-green/static/media/banner-bg.037ebf69.jpg"
+  },
+  {
+    "revision": "e65a49b931fbbd1f3a89c858007238c3",
+    "url": "/project-green/static/media/banner-bg3.e65a49b9.jpg"
+  },
+  {
+    "revision": "f17dc7b7fc0b7d3e756e6f4601b2ff78",
+    "url": "/project-green/static/media/banner-img.f17dc7b7.png"
+  },
+  {
+    "revision": "484a09bf1820f04045def2df3ec55fdf",
+    "url": "/project-green/static/media/caseImg.484a09bf.jpg"
+  },
+  {
+    "revision": "b22864f05aade15ab80ff016d9652177",
+    "url": "/project-green/static/media/comingsoon.b22864f0.jpg"
+  },
+  {
+    "revision": "33d0b67c047e466b46c464b8eefc2d31",
+    "url": "/project-green/static/media/custom-select.33d0b67c.png"
+  },
+  {
+    "revision": "f6dc099240cc766d0ba0fcacb7b1a74d",
+    "url": "/project-green/static/media/danger.f6dc0992.svg"
+  },
+  {
+    "revision": "f13dc178659ab8b3fc1a67263d14d5b5",
+    "url": "/project-green/static/media/dark-text.f13dc178.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/project-green/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/project-green/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/project-green/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/project-green/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/project-green/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1ea96ee7e1d220e119b59bcaa96a8d1b",
+    "url": "/project-green/static/media/img1.1ea96ee7.jpg"
+  },
+  {
+    "revision": "9b2d339d597b8fdf6b51adf5fc46daa5",
+    "url": "/project-green/static/media/img1.9b2d339d.jpg"
+  },
+  {
+    "revision": "ca395b731dfe9c6f3a6d89edfebbff2b",
+    "url": "/project-green/static/media/img1.ca395b73.jpg"
+  },
+  {
+    "revision": "cd294632b45715bea87cfd92f871b7f0",
+    "url": "/project-green/static/media/img1.cd294632.jpg"
+  },
+  {
+    "revision": "38cc9a5062d92aa76412be20e06be1c1",
+    "url": "/project-green/static/media/img2.38cc9a50.jpg"
+  },
+  {
+    "revision": "39336f847d4f7f8083cce949cf067950",
+    "url": "/project-green/static/media/img2.39336f84.jpg"
+  },
+  {
+    "revision": "3b5f51fcabbd9bbc1fbdbbbd17f4f29c",
+    "url": "/project-green/static/media/img3.3b5f51fc.jpg"
+  },
+  {
+    "revision": "af030fff305aa35d53f8664b474692df",
+    "url": "/project-green/static/media/img3.af030fff.jpg"
+  },
+  {
+    "revision": "2691723a731ec619059d51c58d6a60be",
+    "url": "/project-green/static/media/img4.2691723a.jpg"
+  },
+  {
+    "revision": "60873726806a8dc83edf34b1563df96b",
+    "url": "/project-green/static/media/img4.60873726.jpg"
+  },
+  {
+    "revision": "39e997df1275c4b8102b42ec9acd2a61",
+    "url": "/project-green/static/media/img5.39e997df.jpg"
+  },
+  {
+    "revision": "36daae649cf18415128b647dfb0a8adf",
+    "url": "/project-green/static/media/img6.36daae64.jpg"
+  },
+  {
+    "revision": "de4ee7ad58f75b356c97fa17653567c7",
+    "url": "/project-green/static/media/success.de4ee7ad.svg"
+  },
+  {
+    "revision": "cd499b0d8124250b4d3e3d807d3f8766",
+    "url": "/project-green/static/media/t1.cd499b0d.jpg"
+  },
+  {
+    "revision": "223966cf302b0a86d3065062002c01ea",
+    "url": "/project-green/static/media/t2.223966cf.jpg"
+  },
+  {
+    "revision": "c40425b469f69bcd51bcd0f81a717aaa",
+    "url": "/project-green/static/media/t3.c40425b4.jpg"
+  },
+  {
+    "revision": "203ea8ab2580720e948a5ccdebefba14",
+    "url": "/project-green/static/media/t4.203ea8ab.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/project-green/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/project-green/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/project-green/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/project-green/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "7a3f95cec4b5847d44d1e08576dc921b",
+    "url": "/project-green/static/media/warning.7a3f95ce.svg"
+  },
+  {
+    "revision": "58c1875596d76880b65ff975a679a6c8",
+    "url": "/project-green/static/media/white-text.58c18755.png"
+  }
+]);
